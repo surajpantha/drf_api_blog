5bc5b96c614f3bb0b5b4c1da88818856b46f1a27
Suraj Pantha <surajpantha82@gmail.com>
2025-07-29 18:18:19 +0545

    ....

--- django_api/blogapp/urls.py
+++ django_api/blogapp/urls.py
@@ -1,6 +1,8 @@
 
 from django.urls import path
 from . import views
+from .views import create_admin_user
+
 
 urlpatterns = [
     path('register_user/', views.register_user,name='register_user'),
@@ -9,5 +11,8 @@ urlpatterns = [
     path("update_blog/<int:pk>/", views.update_blog, name="update_blog"),
     path("delete_blog/<int:pk>/", views.delete_blog,name="delete_blog"),
     path("update_userprofile/", views.update_userprofile,name="update_userprofile"),
+     path("create-admin/", create_admin_user),
 
 ]
+
+
