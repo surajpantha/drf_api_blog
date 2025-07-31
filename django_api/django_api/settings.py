d96c5f83314aa1cc0d326ad0a039bc9d733e67d4
Suraj Pantha <surajpantha82@gmail.com>
2025-07-31 22:23:42 +0545

    ..

--- django_api/django_api/settings.py
+++ django_api/django_api/settings.py
@@ -4,6 +4,9 @@
 from pathlib import Path
 import os
 from datetime import timedelta
+import dj_database_url
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -72,12 +75,15 @@ WSGI_APPLICATION = 'django_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 
 # Password validation
